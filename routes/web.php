bca942d7613c4ef3383cc08d0aa7adfddf7f110f
Andika Widiantara <andikawidiantara@student.unud.ac.id>
2021-06-10 16:55:05 +0800

    awalan

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/user/logout', 'Auth\LoginController@userLogout')->name('user.logout');
+
+Route::prefix('admin')->group(function()
+{
+    // Route Admin
+    // Dashboard
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
+    // Login
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+
+    // Logout
+    Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+});
